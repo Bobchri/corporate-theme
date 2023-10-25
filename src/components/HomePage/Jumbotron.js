68cae40f9a15a703e526541b3a52ef413188d513
Borneel B. Phukan <borneelphukan@gmail.com>
2023-10-25 04:27:58 +0200

    refactor

--- src/components/HomePage/Jumbotron.js
+++ src/components/HomePage/Jumbotron.js
@@ -11,12 +11,12 @@ return (
         className="w-max h-64 md:w-max md:h-[25vw]"
         src={jumbotron}
         alt="Background"
-        width={500}
-        height={500}
+        width={1000}
+        height={1000}
         />
         
         {/*Info banner*/}
-        <div className="w-max h-max my-auto max-w-xl rounded overflow-hidden shadow-lg bg-white py-10 -mx-16 px-5">
+        <div className="h-max my-auto max-w-xl rounded shadow-lg bg-white py-10 -mx-16 px-5">
         <p className="text-center text-base md:text-lg lg:text-orange-500">WHAT WE DO</p>
         <h5 className="text-2xl text-center font-semibold m-3">Need a solid theme you can count on? Look no further!</h5>
         <p className=" max-w-md mx-auto text-base md:text-lg lg:text-neutral-400 ">We create interactive, modern websites. We help you build strong online presence for your business by creating a professional website which best suits your needs and target audience.</p>
