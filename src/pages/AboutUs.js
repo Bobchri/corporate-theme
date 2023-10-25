b4871b5be14826168561e74ca64f6196f63c502f
Borneel B. Phukan <borneelphukan@gmail.com>
2023-10-25 14:39:08 +0200

    major commit

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,31 +1,17 @@
 "use client";
 import React from "react";
 import DefaultLayout from "@/layout/DefaultLayout";
+import OurTeam from "@/components/AboutUs/OurTeam";
+import Services from "@/components/AboutUs/Services";
+import ChooseUs from "@/components/AboutUs/ChooseUs";
 
 const AboutUs = () => {
   return (
     <DefaultLayout>
       <div className="p-5">
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
-        <h1>About Us</h1>
+        <ChooseUs/>
+        <Services/>
+        <OurTeam/>
       </div>
     </DefaultLayout>
   );
