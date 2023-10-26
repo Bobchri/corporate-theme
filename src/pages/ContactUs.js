7bec8b13382d13d5bf42dba949143ffc8fbaed1e
Borneel B. Phukan <borneelphukan@gmail.com>
2023-10-26 17:48:17 +0200

    refactor

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,13 +2,11 @@
 import React from "react";
 import DefaultLayout from "@/layout/DefaultLayout";
 import DetailsStack from "@/components/ContactUs/DetailsStack";
-import ContactBreadCrumb from "@/components/breadcrumbs/contactBreadCrumb";
 
 const ContactUs = () => {
   return (
     <DefaultLayout>
       <div className="p-5">
-        <ContactBreadCrumb />
         <DetailsStack />
       </div>
     </DefaultLayout>
