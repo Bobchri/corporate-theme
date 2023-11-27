e3a602a1bb14466be8586a1dc7008058fe8e8a83
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-27 01:03:48 +0100

    jumbotron fixed

--- src/components/HomePage/Jumbotron.js
+++ src/components/HomePage/Jumbotron.js
@@ -5,36 +5,40 @@ import Button from "../buttons/Button";
 
 const Jumbotron = () => {
   return (
-    <div className="container mx-auto overflow-hidden flex grid-cols-2 md:grid-cols-2 lg:grid-cols-2 p-10">
-      {/*Jumbotron Background*/}
-      <Image
-        className="w-max h-64 md:w-max md:h-[25vw]"
-        src={jumbotron}
-        alt="Background"
-        width={1000}
-        height={1000}
-      />
+    <div className="container mx-auto flex flex-col md:flex-row items-center p-10">
+      {/* Jumbotron Background */}
+      <div className="w-full md:w-1/2 md:order-1">
+        <Image
+          className="w-full h-auto md:h-[25vw]"
+          src={jumbotron}
+          alt="Background"
+          width={1000}
+          height={1000}
+        />
+      </div>
 
-      {/*Info banner*/}
-      <div className="h-max my-auto max-w-xl rounded shadow-lg bg-white py-10 -mx-16 px-5">
-        <p className="text-center text-base md:text-lg lg:text-orange-500">
-          WHAT WE DO
-        </p>
-        <h5 className="text-2xl text-center font-semibold m-3">
-          Need a solid theme you can count on? Look no further!
-        </h5>
-        <p className=" max-w-md mx-auto text-base md:text-lg lg:text-neutral-400 ">
-          We create interactive, modern websites. We help you build strong
-          online presence for your business by creating a professional website
-          which best suits your needs and target audience.
-        </p>
-        <div className="text-center">
-          <Button
-            bgColor="white"
-            textColor="black"
-            text="View More"
-            className="text-center"
-          />
+      {/* Info banner */}
+      <div className="w-full md:w-1/2 md:order-2 md:ml-5">
+        <div className="h-max rounded shadow-lg bg-white py-10 px-5">
+          <p className="text-center text-base md:text-lg lg:text-orange-500">
+            WHAT WE DO
+          </p>
+          <h5 className="text-2xl text-center font-semibold m-3">
+            Need a solid theme you can count on? Look no further!
+          </h5>
+          <p className="max-w-md mx-auto text-base md:text-lg lg:text-neutral-400">
+            We create interactive, modern websites. We help you build a strong
+            online presence for your business by creating a professional website
+            that best suits your needs and target audience.
+          </p>
+          <div className="text-center">
+            <Button
+              bgColor="white"
+              textColor="black"
+              text="View More"
+              className="text-center"
+            />
+          </div>
         </div>
       </div>
     </div>
