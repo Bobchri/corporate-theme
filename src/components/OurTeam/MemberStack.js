44fe3d1830afdf4b85f8356b540e0f46267f4b54
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-24 01:28:47 +0100

    mongodb connected

--- src/components/OurTeam/MemberStack.js
+++ src/components/OurTeam/MemberStack.js
@@ -1,19 +1,34 @@
-import React from "react";
-import ourTeamData from "@/data/ourteam-data.json"; // Update the path accordingly
+import React, { useState, useEffect } from "react";
 import MembersCard from "../card/MembersCard.js";
 
 const MemberStack = () => {
+  const [teamData, setTeamData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/mongo"); // Assuming you have set up the API route as mentioned in the previous example
+        const data = await response.json();
+        setTeamData(data.teamData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs once when the component mounts
+
   return (
     <div className="container mx-auto mb-20">
       <div className="max-w-8xl mx-auto text-center text-lg p-5 mt-10">
         <div className="px-auto overflow-hidden grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-8 p-1">
-          {ourTeamData.map((data, index) => (
+          {teamData.map((member, index) => (
             <MembersCard
               key={index}
-              avatar={data.avatar}
-              role={data.role}
-              name={data.name}
-              description={data.description}
+              avatar={member.avatar}
+              role={member.role}
+              name={member.name}
+              description={member.description}
             />
           ))}
         </div>
