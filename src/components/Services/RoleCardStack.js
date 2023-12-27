48a29cffd54ccbdea9200163e53a23c08fd8a2ba
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-27 22:41:10 +0100

    reverted to ca0b26a6c13a079c43458f21c5adf146be79a905

--- src/components/Services/RoleCardStack.js
+++ src/components/Services/RoleCardStack.js
@@ -1,27 +1,11 @@
 import React from "react";
 import RoleCard from "../card/RoleCard";
-import { useState } from "react";
-import { useEffect } from "react";
+import roleCard from "@/data/role-card-data.json"; // Update the path accordingly
 
 const RoleCardStack = () => {
-  const [roleData, setRoleData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/role");
-        const data = await response.json();
-        setRoleData(data.roleData);
-      } catch (error) {
-        console.error("Error fetching data", error);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
     <div className="container my-5 px-auto overflow-hidden mx-auto -m-16 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8 p-5">
-      {roleData.map((data, index) => (
+      {roleCard.map((data, index) => (
         <RoleCard
           key={index}
           avatar={data.avatar}
